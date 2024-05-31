a58ffa8cab77302ec9233fa599ee0b426e200139
Leonardo Caldeira <leonardo.caldeira@cronosbank.com.br>
2024-05-31 20:10:54 -0300

    feat(drawer): continuing the drawer implementation

--- lib/app/shared/widgets/drawer_custom.dart
+++ lib/app/shared/widgets/drawer_custom.dart
@@ -56,29 +56,178 @@ class _DrawerCustomState extends State<DrawerCustom> {
                   ],
                 ),
               ),
-              ListTile(
-                title: const Text('Home'),
-                selected: widget.selectedIndex == 0,
-                onTap: () {
-                  widget.onItemTapped(0);
-                  Scaffold.of(contextBuilder).openEndDrawer();
-                },
+              // NavigationRail(
+              //   destinations: const <NavigationRailDestination>[
+              //     NavigationRailDestination(
+              //       icon: Icon(Icons.favorite_border),
+              //       selectedIcon: Icon(Icons.favorite),
+              //       label: Text('First'),
+              //     )
+              //   ],
+              //   selectedIndex: widget.selectedIndex,
+              // ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  title: const Text('Início'),
+                  leading: const Icon(Icons.home_outlined),
+                  selected: widget.selectedIndex == 0,
+                  onTap: () {
+                    widget.onItemTapped(0);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
               ),
-              ListTile(
-                title: const Text('Business'),
-                selected: widget.selectedIndex == 1,
-                onTap: () {
-                  widget.onItemTapped(1);
-                  Scaffold.of(contextBuilder).openEndDrawer();
-                },
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.explore),
+                  title: const Text('Shorts'),
+                  selected: widget.selectedIndex == 1,
+                  onTap: () {
+                    widget.onItemTapped(1);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
               ),
-              ListTile(
-                title: const Text('School'),
-                selected: widget.selectedIndex == 2,
-                onTap: () {
-                  widget.onItemTapped(2);
-                  Scaffold.of(contextBuilder).openEndDrawer();
-                },
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.subscriptions_outlined),
+                  title: const Text('inscrições'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  title: const Text('Você'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.insert_chart_outlined_rounded),
+                  title: const Text('Seu canal'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.history),
+                  title: const Text('Histórico'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.live_tv_sharp),
+                  title: const Text('Playlist'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Configurações'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.live_tv_sharp),
+                  title: const Text('Histórico de denuncias'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.live_tv_sharp),
+                  title: const Text('Ajuda'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.live_tv_sharp),
+                  title: const Text('Enviar feedback'),
+                  selected: widget.selectedIndex == 2,
+                  onTap: () {
+                    widget.onItemTapped(2);
+                    Scaffold.of(contextBuilder).openEndDrawer();
+                  },
+                ),
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+                color: Colors.grey,
               ),
             ],
           );
